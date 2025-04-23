165bc1eacfae9e40b096178a92fc99c4641ef9b5
btrojan-official <bartosztrojanofficial@gmail.com>
2025-04-23 17:52:11 +0200

    style: color of error

--- app/src/views/RegisterView.vue
+++ app/src/views/RegisterView.vue
@@ -107,6 +107,7 @@ export default {
         width: fit-content;
         background-color: rgb(255, 178, 178);
         border: 2px solid rgb(255, 92, 92);
+        color: rgb(255, 92, 92);
         border-radius: 10px;
         padding: 10px;
         width: 100%;
